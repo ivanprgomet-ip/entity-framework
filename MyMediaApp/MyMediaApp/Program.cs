c154dc889b86a7e70414dbc84bd2e77acfe1ea85
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-10-18 13:09:01 +0200

    querying books, papers and movies

--- MyMediaApp/MyMediaApp/Program.cs
+++ MyMediaApp/MyMediaApp/Program.cs
@@ -24,9 +24,41 @@ namespace MyMediaApp
                 }
             });
             mc.Papers.Add(new Paper() { type = PaperType.Essay, Author = "Ralph Waldo Emerson", Name = "Fate" });
+            //mc.SaveChanges();
 
-            mc.SaveChanges();
-            Console.WriteLine("Changes saved");
+
+            QueryBooks(mc);
+            QueryMovies(mc);
+            QueryPapers(mc);
+
+
         }
+        public static void QueryBooks(MediaContext context)
+        {
+            foreach (var book in context.Books)
+            {
+                Console.WriteLine($"'{book.Name}' - {book.Author} ({book.Genre}, {book.Pages})");
+            }
+        }
+        public static void QueryMovies(MediaContext context)
+        {
+            foreach (var movie in context.Movies)
+            {
+                Console.WriteLine($"'{movie.Name}' - {movie.Director} ({movie.LengthMinutes})");
+                //foreach (KeyValuePair<string,string> kvp in movie.Top3Actors)
+                //{
+                //    Console.WriteLine(kvp.Key+" "+kvp.Value);
+                //}
+            }
+        }
+        public static void QueryPapers(MediaContext context)
+        {
+            foreach (var paper in context.Papers)
+            {
+                Console.WriteLine($"'{paper.Name}' - {paper.Author} ({paper.type})");
+            }
+        }
+
+
     }
 }
