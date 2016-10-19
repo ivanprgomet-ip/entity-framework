d96f20fefd811ce91023fb51628466a1a877d0ba
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-10-19 17:00:24 +0200

    added simple query of adventureworks database (after using database first approach to create entity classes) for demonstrational purposes

--- Laboration1_2/DatabaseFirst/Program.cs
+++ Laboration1_2/DatabaseFirst/Program.cs
@@ -10,7 +10,11 @@ namespace DatabaseFirst
     {
         static void Main(string[] args)
         {
-
+            AdventureWorks2014Entities a = new AdventureWorks2014Entities();
+            foreach (var employee in a.Employees)
+            {
+                Console.WriteLine("Fullname: "+employee.Person.FirstName + " " + employee.Person.MiddleName + " " + employee.Person.LastName+" | Marital Status: "+employee.MaritalStatus);
+            }
         }
     }
 }
