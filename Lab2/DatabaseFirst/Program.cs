025503be6be95780d0591f9e14d7d9213ac69f77
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-10-20 11:02:38 +0200

    assignment completion with query in main method based on titles written by authors

---
+++ Lab2/DatabaseFirst/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BooksEntities be = new BooksEntities();
+
+            //foreach (var author in be.Authors)
+            //{
+            //    Console.WriteLine("______________");
+            //    Console.WriteLine("Titles by "+author.FirstName+" "+author.LastName);
+            //    foreach (var title in author.Titles)
+            //    {
+            //        Console.WriteLine(title.Title1+" "+title.EditionNumber+" "+title.ISBN+" "+title.Copyright);
+            //    }
+            //    Console.WriteLine("______________");
+            //}
+
+            foreach (var title in be.Titles)
+            {
+                Console.WriteLine("______________");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("'"+title.Title1 +"' by: ");
+                Console.ResetColor();
+                foreach (var author in title.Authors)
+                {
+                    Console.WriteLine(author.FirstName+" "+author.LastName);
+                }
+                Console.WriteLine("______________");
+            }
+        }
+    }
+}
