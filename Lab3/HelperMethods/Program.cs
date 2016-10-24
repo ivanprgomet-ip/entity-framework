ddb68f5bccf141aea5c1dffef48ab5b9abd2a34c
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-10-24 09:32:13 +0200

    lab 3 sn instantiated. helper class exercise done

---
+++ Lab3/HelperMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ett tal: ");
+            float f = HelperClass.StringToFloat(Console.ReadLine());
+            Console.WriteLine($"{f} * 12 = {f * 12}");
+        }
+    }
+    public static class HelperClass
+    {
+
+        public static float StringToFloat(string str)
+        {
+            float output;
+            bool isString = float.TryParse(str, out output);
+            if (isString)
+                return output;
+            else
+                return 0;
+        }
+    }
+}
