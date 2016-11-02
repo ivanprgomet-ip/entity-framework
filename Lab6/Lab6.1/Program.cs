5a72cd927d0848589672f300324ad5869a7f9529
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-11-02 17:12:46 +0100

    removed the student id from student declarations also

--- Lab6/Lab6.1/Program.cs
+++ Lab6/Lab6.1/Program.cs
@@ -10,66 +10,66 @@ namespace Lab6._1
     {
         static void Main(string[] args)
         {
-            Course EntityFrameworkCourse = new Course() { CourseID = 101, CourseName = "Entity Framework", Credits = 50 };
-            Course DotNetFrameworkCourse= new Course() { CourseID = 102, CourseName = ".NET Framework", Credits = 50 };
-            Course JavascriptCourse = new Course() { CourseID = 103, CourseName = "Javascript", Credits = 50 };
+            Course EntityFrameworkCourse = new Course() {CourseName = "Entity Framework", Credits = 50 };
+            Course DotNetFrameworkCourse= new Course() {CourseName = ".NET Framework", Credits = 50 };
+            Course JavascriptCourse = new Course() {CourseName = "Javascript", Credits = 50 };
 
-            Course HttpCourse = new Course() { CourseID = 121, CourseName = "Http", Credits = 100 };
-            Course SecurityManagementCourse = new Course() { CourseID = 122, CourseName = "Security Management", Credits = 50};
+            Course HttpCourse = new Course() {CourseName = "Http", Credits = 100 };
+            Course SecurityManagementCourse = new Course() {CourseName = "Security Management", Credits = 50};
 
 
             #region student initialization
-            Student s1 = new Student() { FirstMidName = "Ivan", LastName = "Prgomet", ID = 1, EnrollmentDate = new DateTime(2015, 12, 24) };
+            Student s1 = new Student() { FirstMidName = "Ivan", LastName = "Prgomet",EnrollmentDate = new DateTime(2015, 12, 24) };
             s1.Enrollments = new List<Enrollment>
             {
                 /*
                     three different courses on the enrollment of 'webdeveloper within .NET'
                 */
                 new Enrollment() {
-                    EnrollmentID =1,
+                    
                     EnrollmentName ="Webdeveloper within .NET",
                     Course = EntityFrameworkCourse,
                     Grade ="A-F" },
                 new Enrollment() {
-                    EnrollmentID =1,
+           
                     EnrollmentName ="Webdeveloper within .NET",
                     Course = DotNetFrameworkCourse,
                     Grade ="A-F" },
                 new Enrollment() {
-                    EnrollmentID =1,
+          
                     EnrollmentName ="Webdeveloper within .NET",
                     Course = JavascriptCourse,
                     Grade ="A-F" },
             };
-            Student s2 = new Student() { FirstMidName = "Lea", LastName = "Winchester", ID = 2, EnrollmentDate = new DateTime(2015, 12, 24) };
+            Student s2 = new Student() { FirstMidName = "Lea", LastName = "Winchester",EnrollmentDate = new DateTime(2015, 12, 24) };
             s2.Enrollments = new List<Enrollment>
             {
                 new Enrollment() {
-                    EnrollmentID =1,
+        
                     EnrollmentName ="Webdeveloper within .NET",
                     Course = EntityFrameworkCourse,
                     Grade ="A-F" },
                 new Enrollment() {
-                    EnrollmentID =1,
+
                     EnrollmentName ="Webdeveloper within .NET",
                     Course = DotNetFrameworkCourse,
                     Grade ="A-F" },
                 new Enrollment() {
-                    EnrollmentID =1,
+
                     EnrollmentName ="Webdeveloper within .NET",
                     Course = JavascriptCourse,
                     Grade ="A-F" },
             };
-            Student s3 = new Student() { FirstMidName = "Jason", LastName = "Bourne", ID = 3, EnrollmentDate = new DateTime(2016, 12, 24) };
+            Student s3 = new Student() { FirstMidName = "Jason", LastName = "Bourne",EnrollmentDate = new DateTime(2016, 12, 24) };
             s3.Enrollments = new List<Enrollment>
             {
                 new Enrollment() {
-                    EnrollmentID =1,
+
                     EnrollmentName ="Network Security",
                     Course = HttpCourse,
                     Grade ="A,F" },
                 new Enrollment() {
-                    EnrollmentID =1,
+
                     EnrollmentName ="Network Security",
                     Course = SecurityManagementCourse,
                     Grade ="A-C" },
