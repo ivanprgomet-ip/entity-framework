f1bcbfcabd5eb807af193ad6121c57dd5a146252
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-11-07 13:17:58 +0100

    customers listed

---
+++ MovieNight/WebGUI/pages/allcustomers.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="allcustomers.aspx.cs" Inherits="WebGUI.pages.allcustomers" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <article id="customerslisted" runat="server">
+
+        </article>
+        <br />
+        <br />
+        <a href="../index.html">return to main menu</a>
+    </div>
+    </form>
+</body>
+</html>
---
+++ MovieNight/WebGUI/pages/allcustomers.aspx.cs
@@ -0,0 +1,31 @@
+﻿using EFlib;
+using EFlib.BLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebGUI.pages
+{
+    public partial class allcustomers : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            List<Customer> customers = BLLCustomer.ReturnAllCustomers();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("All Registered Customers:");
+            sb.Append("<br/><br/>");
+            sb.Append("..............................");
+            sb.Append("<br/><br/>");
+            foreach (var customer in customers)
+            {
+                sb.Append(string.Format($"CustomerID: {customer.CustomerID} <br/>CustomerName: {customer.CustomerName} <br/>CustomerAddress: {customer.CustomerAdress} <br/>CustomerPhone: {customer.CustomerPhone}"));
+                sb.Append("<br/><br/>");
+            }
+            sb.Append("..............................");
+            customerslisted.InnerHtml = sb.ToString();
+        }
+    }
+}
