41296c364562679ea6c33db09bf2255ea8d69bce
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-11-07 11:30:01 +0100

    all movies being listed

---
+++ MovieNight/WebGUI/pages/allmovies.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="allmovies.aspx.cs" Inherits="WebGUI.pages.allmovies" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <article id="movieslisted" runat="server">
+
+        </article>
+        <br />
+        <br />
+        <a href="../index.html">return to main menu</a>
+    </div>
+    </form>
+</body>
+</html>
---
+++ MovieNight/WebGUI/pages/allmovies.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.UI;
+using System.Collections.Generic;
+using EFlib;
+using EFlib.BLL;
+using System.Text;
+
+namespace WebGUI.pages
+{
+    public partial class allmovies : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //retrieve all movies and list them in the html innertext element
+            List<Movie> movies = BLLMovie.ReturnAllMovies();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("All Movies:");
+            sb.Append("<br/><br/>");
+            sb.Append("..............................");
+            sb.Append("<br/><br/>");
+            foreach (var movie in movies)
+            {
+                sb.Append(string.Format($"Title: {movie.MovieName} <br/>Director: {movie.MovieDirector} <br/>Genre: {movie.Genre.GenreName}"));
+                sb.Append("<br/><br/>");
+            }
+            sb.Append("..............................");
+            movieslisted.InnerHtml = sb.ToString();
+        }
+    }
+}
